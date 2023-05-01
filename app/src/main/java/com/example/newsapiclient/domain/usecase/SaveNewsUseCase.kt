07bc59a24f361420459cec3eee4280e06d66be1d
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-01 14:35:46 +0200

    domain layer completed

--- app/src/main/java/com/example/newsapiclient/domain/usecase/SaveNewsUseCase.kt
+++ app/src/main/java/com/example/newsapiclient/domain/usecase/SaveNewsUseCase.kt
@@ -1,4 +1,8 @@
 package com.example.newsapiclient.domain.usecase
 
-class SaveNewsUseCase {
+import com.example.newsapiclient.data.model.Article
+import com.example.newsapiclient.domain.repository.NewsRepository
+
+class SaveNewsUseCase(private val newsRepository: NewsRepository) {
+    suspend fun execute(article: Article) = newsRepository.saveNews(article)
 }
