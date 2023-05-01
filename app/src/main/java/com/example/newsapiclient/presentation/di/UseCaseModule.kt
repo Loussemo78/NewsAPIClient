21578a3477abc14ae4ec5a1754e6468e0b88ba91
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-01 16:35:40 +0200

    module implementation in progress

---
+++ app/src/main/java/com/example/newsapiclient/presentation/di/UseCaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.newsapiclient.presentation.di
+
+import com.example.newsapiclient.domain.repository.NewsRepository
+import com.example.newsapiclient.domain.usecase.GetNewsHeadLinesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class UseCaseModule {
+
+    @Singleton
+    @Provides
+    fun provideGetNewsHeadLinesUseCase(
+        newsRepository: NewsRepository
+    ):GetNewsHeadLinesUseCase{
+        return GetNewsHeadLinesUseCase(newsRepository)
+    }
+
+
+}
