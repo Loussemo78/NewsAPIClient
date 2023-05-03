1f1c3fbf6434d38efcefdb3a67079c6eb7c3c449
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-03 11:23:33 +0200

    viewBinding and nav_graph added

--- app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
