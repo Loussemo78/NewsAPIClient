8e3985cae9ac63c59b939e98e7b66cd965e909b9
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-04 12:44:08 +0200

    NewsFragment completed

--- app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.newsapiclient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiclient.databinding.ActivityMainBinding
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         binding.bnvNews.setupWithNavController(
             navController
         )
+        viewModel = ViewModelProvider(this,factory)
+            .get(NewsViewModel::class.java)
 
     }
 
