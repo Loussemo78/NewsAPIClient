1be16cd0864191af5e947c6073735aede75b7df2
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-04 14:03:12 +0200

    NewViewModelFactory updated

--- app/src/main/java/com/example/newsapiclient/presentation/viewmodel/NewsViewModelFactory.kt
+++ app/src/main/java/com/example/newsapiclient/presentation/viewmodel/NewsViewModelFactory.kt
@@ -4,15 +4,18 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.newsapiclient.domain.usecase.GetNewsHeadLinesUseCase
+import com.example.newsapiclient.domain.usecase.GetSearchedNewsUseCase
 
 class NewsViewModelFactory(
     private val app: Application,
-    val getNewsHeadLinesUseCase: GetNewsHeadLinesUseCase
+    private val getNewsHeadLinesUseCase: GetNewsHeadLinesUseCase,
+    private val getSearchedNewsUseCase: GetSearchedNewsUseCase
 ):ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(
             app,
-            getNewsHeadLinesUseCase
+            getNewsHeadLinesUseCase,
+            getSearchedNewsUseCase
         ) as T
     }
 }
